ab90508a83d5d8a4a88ebf382518a967ec85190f
Gennadii Kruglov <kruglov.gennadii@gmail.com>
2020-08-13 14:39:01 +0300

    Add OrderTableUpdated migration

---
+++ Store.DataAccess/Migrations/20200813111917_OrderTableUpdated.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.DataAccess.Migrations
+{
+    public partial class OrderTableUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "PaymentId",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "PaymentId",
+                table: "Orders",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
