c40cb2d8ad023dc1316e4c8082c4cc6aa701ff71
Gennadii Kruglov <kruglov.gennadii@gmail.com>
2020-08-14 17:54:36 +0300

    Add Controllers to Presentation Layer

---
+++ Store.Presentation/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.BusinessLogic.Services.Interfaces;
+using Store.DataAccess.Entities;
+using System.Threading.Tasks;
+
+namespace Store.Presentation.Controllers
+{
+    [ApiController]
+    [Route("users/")]
+    public class UserController : Controller
+    {
+        
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userService.GetUsers());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int userId)
+        {
+            var result = _userService.GetUserById(userId);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post(ApplicationUser user)
+        {
+            _userService.AddUser(user);
+
+            return Ok(user);
+        }
+
+    }
+}
