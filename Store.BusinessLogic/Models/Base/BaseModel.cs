91d03f60cd6d47d4f8b59c9058e60ec51a69d3f8
Gennadii Kruglov <kruglov.gennadii@gmail.com>
2020-08-17 17:58:58 +0300

    Add BaseModel and UserModel

---
+++ Store.BusinessLogic/Models/Base/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Store.BusinessLogic.Models.Base
+{
+    public class BaseModel
+    {
+        public long Id { get; set; }
+        public ICollection<string> Errors = new List<string>();
+    }
+}
---
+++ Store.BusinessLogic/Models/Users/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using Store.BusinessLogic.Models.Base;
+using System.Collections.Generic;
+
+namespace Store.BusinessLogic.Models.Users
+{
+    public class UserModel : BaseModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<string> Roles { get; set; }
+
+    }
+}
