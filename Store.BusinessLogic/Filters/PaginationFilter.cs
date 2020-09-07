306254171b06694dbee39cacbadb842a6abe50ad
Gennadii Kruglov <kruglov.gennadii@gmail.com>
2020-09-07 15:43:43 +0300

    Update Pagination filter

--- Store.BusinessLogic/Filters/PaginationFilter.cs
+++ Store.BusinessLogic/Filters/PaginationFilter.cs
@@ -2,18 +2,34 @@
 {
     public class PaginationFilter
     {
-        public const int PageNumberByDefault = 1;
-        public const int PageSizeByDefault = 10;
-
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-
-        public PaginationFilter()
+        // Default values 
+        private int _pageNumberByDefault = 1;
+        private int _pageSizeByDefault = 10;
+        
+        public int PageNumber
         {
-            // Default view set
-            PageNumber = PageNumberByDefault;
-            PageSize = PageSizeByDefault;
+            get
+            {
+                return _pageNumberByDefault;
+            }
+            set
+            {
+                // Value assignment, where values can not be less than 1
+                _pageNumberByDefault = (value < 1) ? _pageNumberByDefault : value;
+            }
         }
 
+        public int PageSize
+        {
+            get
+            {
+                return _pageSizeByDefault;
+            }
+            set
+            {
+                // Value assignment, where values can not be less than 1
+                _pageSizeByDefault = (value < 1) ? _pageSizeByDefault : value;
+            }
+        }
     }
 }
